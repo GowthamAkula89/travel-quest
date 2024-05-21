9c98e221c014e6c70483ca999e5ed01f73e5879a
GowthamAkula89 <akulagowthamroyal@gmail.com>
2024-05-21 22:40:35 +0530

    Add Header

---
+++ src/Components/Header/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./header.css";
+import { PiAirplaneTakeoffLight } from "react-icons/pi";
+
+const Header = () => {
+    return(
+        <div className="header">
+            <div className="header-title">
+                <div className="header-logo">
+                    <PiAirplaneTakeoffLight className="logo-img"/>
+                    <div className="logo-title"><span className="logo-name">Travel</span> Quest</div>
+                </div>
+                <div className="vertical-bar"></div>
+                <div className="header-text">Private tailor-made journeys of a lifetime</div>
+            </div>
+            <div className="header-contact">
+                <div className="constact-number">1 888 263 2574</div>
+                <button type="submit" className="btn">START PLANNING</button>
+            </div>
+        </div>
+    )
+}
+export default Header;
