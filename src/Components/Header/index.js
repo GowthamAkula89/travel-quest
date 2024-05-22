b1fa45d428c876c1ea4abcdbd5b19342186de45b
GowthamAkula89 <akulagowthamroyal@gmail.com>
2024-05-22 10:25:38 +0530

    fix: header Implementation

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,7 +1,7 @@
-import React from "react";
+import React, {useState} from "react";
 import "./header.css";
 import { PiAirplaneTakeoffLight } from "react-icons/pi";
-
+import ModalComponent from "../ModalComponent";
 export function Logo(){
     return (
         <div className="header-logo">
@@ -11,7 +11,13 @@ export function Logo(){
     );
 }
 const Header = () => {
+    const [showModal, setShowModal] = useState(false);
+
+    const handleCreateTrip = () =>{
+        setShowModal(true);
+    }
     return(
+        <>
         <div className="header">
             <div className="header-title">
                 {Logo()}
@@ -20,9 +26,12 @@ const Header = () => {
             </div>
             <div className="header-contact">
                 <div className="constact-number">1 888 263 2574</div>
-                <button type="submit" className="btn">START PLANNING</button>
+                <button type="submit" className="btn" onClick={handleCreateTrip}>START PLANNING</button>
             </div>
+            
         </div>
+        <ModalComponent showModal={showModal} setShowModal={setShowModal}/>
+        </>
     )
 }
 export default Header;
